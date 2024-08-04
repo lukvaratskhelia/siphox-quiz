b0ec59e74c539637d52bff9ffe5640b98770beaa
lukvaratskhelia <luka.kvaratskhelia@wolterskluwer.com>
2024-08-04 15:38:40 +0400

    initialize project

--- apps/siphox-quiz/src/app/app.tsx
+++ apps/siphox-quiz/src/app/app.tsx
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="siphox-quiz" />
-    </div>
-  );
+  return <div />;
 }
 
 export default App;

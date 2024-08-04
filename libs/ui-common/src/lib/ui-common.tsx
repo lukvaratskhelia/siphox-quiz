8c8eef7ba73448f072df64208085a1e7bb0c2e79
lukvaratskhelia <luka.kvaratskhelia@wolterskluwer.com>
2024-08-04 15:53:10 +0400

    add helper libraries

---
+++ libs/ui-common/src/lib/ui-common.tsx
@@ -0,0 +1,11 @@
+import styles from './ui-common.module.scss';
+
+export function UiCommon() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UiCommon!</h1>
+    </div>
+  );
+}
+
+export default UiCommon;
